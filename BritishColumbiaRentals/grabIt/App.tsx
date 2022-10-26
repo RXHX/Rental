3134252f54fa75f779ce6c994ddedff5477570c0
RXHX <rohan.kediyal@gmail.com>
2022-10-25 21:05:33 -0700

    First Commit

---
+++ BritishColumbiaRentals/grabIt/App.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+const App:FC = () => {
+    return (
+      <SafeAreaView>
+    <Text>Get Set Go!</Text>    
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
---
+++ BritishColumbiaRentals/grabIt/routes/routes.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Route  = () => {
+    const Stack = createStackNavigator();
+    render()
+    {
+       return(
+        <NavigationContainer>
+      <Stack.Navigator>
+          <Stack.Screen name = " " component={'// add component name here'}/> 
+          <Stack.Screen name = " " component={'// add component name here'}/> 
+          <Stack.Screen name = " " component={'// add component name here'}/> 
+          <Stack.Screen name = " " component={'// add component name here'}/> 
+          <Stack.Screen name = " " component={'// add component name here'}/> 
+          <Stack.Screen name = " " component={'// add component name here'}/> 
+          <Stack.Screen name = " " component={'// add component name here'}/> 
+          <Stack.Screen name = " " component={'// add component name here'}/> 
+          <Stack.Screen name = " " component={'// add component name here'}/> 
+          <Stack.Screen name = " " component={'// add component name here'}/> 
+
+      </Stack.Navigator>
+        </NavigationContainer>
+
+       )
+
+
+
+    }
+
+}
+export default Route;
